19eb985883fca98889554cdcd7542b5b606dd74a
ebalog <ed.balogh@acxiom.com>
2019-03-16 13:49:43 -0400

    added validate-object endpoint (PATCH) to validate a provided object against the stored schema

--- controllers/api/v1/package-objects.js
+++ controllers/api/v1/package-objects.js
@@ -6,5 +6,13 @@ const baseRoutes = new BaseRoutes('package-object', 'package-objects', PackageOb
 module.exports = function (router) {
     baseRoutes.buildBasicCrudRoutes(router);
 
-    // custom routes go here
+    router.patch('/:id/validate-object', (req, res) => {
+        baseRoutes.model.validateJson(req.params.id, req.body)
+            .then(isValid => {
+                res.json({ isValid });
+            })
+            .catch(err => {
+                res.status(501).json({error: err});
+            });
+    });
 };
