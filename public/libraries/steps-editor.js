67e4b21a79c59bc85e7a310bf715054f2cfc8df7
dafreels <dafreels@yahoo.com>
2019-04-09 08:24:59 -0400

    - Reworked the parameters form to be consistent across platforms

--- public/libraries/steps-editor.js
+++ public/libraries/steps-editor.js
@@ -124,13 +124,13 @@ function populateStepForm(el) {
     parametersDiv.empty();
     let formDiv;
     let select;
-    let requireButton;
+    let requireCB;
     _.forEach(currentStep.params, (param) => {
         formDiv = createParameterForm();
         formDiv.appendTo(parametersDiv);
         // Decorate the components
         select = formDiv.find('select');
-        requireButton = formDiv.find('#' + formDiv.attr('cbId'));
+        requireCB = formDiv.find('#' + formDiv.attr('cbId'));
         // Set the values
         defaultValues[param.name] = param.defaultValue;
         formDiv.find('input[name="stepParamName"]').val(param.name);
@@ -142,7 +142,7 @@ function populateStepForm(el) {
             select.val(param.type);
         }
         if (param.required) {
-            requireButton.button('toggle');
+            requireCB.attr('checked', true);
         }
     });
 }
@@ -153,13 +153,27 @@ function populateStepForm(el) {
  * @returns Create the parameter row that can be appended to the parameters form.
  */
 function createParameterForm() {
-    const formDiv = $('<div class="row">');
-    $('<div class="col col-sm-1"><label>Name:</label></div>').appendTo(formDiv);
-    $('<div class="col col-sm-3"><input name="stepParamName" type="text"/></div>').appendTo(formDiv);
+    const formDiv = $('<div class="parameter-form">');
+    // Remove button
+    const buttonDiv = $('<div class="pull-right">');
+    buttonDiv.appendTo(formDiv);
+    const button = $('<button class="btn btn-info" title="Remove Parameter">');
+    button.appendTo(buttonDiv);
+    $('<i class="glyphicon glyphicon-minus-sign"></i>').appendTo(button);
+    button.click(function() {
+        formDiv.remove();
+    });
+    // Name
+    let formGroup = $('<div class="form-group">');
+    formGroup.appendTo(formDiv);
+    $('<label>Name:</label>').appendTo(formGroup);
+    $('<input name="stepParamName" type="text" class="form-control"/>').appendTo(formGroup);
+    // Type
+    formGroup = $('<div class="form-group">');
+    formGroup.appendTo(formDiv);
+    $('<label>Type:</label>').appendTo(formGroup);
     const select = $('<select class="form-control param-select">');
-    let column = $('<div class="col col-sm-2">');
-    select.appendTo(column);
-    column.appendTo(formDiv);
+    select.appendTo(formGroup);
     $('<option value="text">Text</option>').appendTo(select);
     $('<option value="boolean">Boolean</option>').appendTo(select);
     $('<option value="integer">Integer</option>').appendTo(select);
@@ -169,21 +183,28 @@ function createParameterForm() {
     $('<option value="script-javascript">Javascript Script</option>').appendTo(select);
     $('<option value="result">Branch Result</option>').appendTo(select);
     $('<option value="object">Object</option>').appendTo(select);
-    const requiredButton = $('<button id="'+ getCustomId('cb') +'" type="button" class="btn btn-info" data-toggle="button" aria-pressed="false">Required</button>');
-    column = $('<div class="col col-sm-2 cb-margin">');
-    requiredButton.appendTo(column);
-    column.appendTo(formDiv);
-    $('<div class="col col-sm-1" style="margin-left: -20px;"><label>Default Value:</label></div>').appendTo(formDiv);
-    column = $('<div class="col col-sm-2">');
-    column.appendTo(formDiv);
-    const defaultValueInput = $('<input name="stepParamDefaultValue" type="text"/>');
-    defaultValueInput.appendTo(column);
-
-    $('<div class="col col-sm-1" style="margin-left: 5px;"><label>Class Name:</label></div>').appendTo(formDiv);
-    column = $('<div class="col col-sm-2">');
-    const className = $('<input name="stepParamClassName" type="text"/>');
-    className.appendTo(column);
-    column.appendTo(formDiv);
+    // Required
+    const cbId = getCustomId('cb');
+    formGroup = $('<div class="checkbox">');
+    formGroup.appendTo(formDiv);
+    const cbLabel = $('<label>');
+    cbLabel.appendTo(formGroup);
+    const requiredCB = $('<input type="checkbox" id="'+ cbId +'">');
+    requiredCB.appendTo(cbLabel);
+    cbLabel.append('Required');
+    // $('Required').appendTo(cbLabel);
+    // Default Value
+    formGroup = $('<div class="form-group">');
+    formGroup.appendTo(formDiv);
+    $('<label>Default Value:</label>').appendTo(formGroup);
+    const defaultValueInput = $('<input name="stepParamDefaultValue" type="text" class="form-control"/>');
+    defaultValueInput.appendTo(formGroup);
+    // Class Name
+    formGroup = $('<div class="form-group">');
+    formGroup.appendTo(formDiv);
+    $('<label>Class Name:</label>').appendTo(formGroup);
+    const className = $('<input name="stepParamClassName" type="text" class="form-control"/>');
+    className.appendTo(formGroup);
 
     defaultValueInput.focusin(function() {
         if (select.val().indexOf('script') === 0) {
@@ -207,15 +228,7 @@ function createParameterForm() {
         }
     });
 
-    column = $('<div class="col col-sm-1" style="margin-left: 5px; margin-bottom: 5px;">');
-    column.appendTo(formDiv);
-    const button = $('<button class="btn btn-info" title="Remove Parameter">');
-    button.appendTo(column);
-    $('<i class="glyphicon glyphicon-minus-sign"></i>').appendTo(button);
-    button.click(function() {
-        formDiv.remove();
-    });
-    formDiv.attr('cbId', requiredButton.attr('id'));
+    formDiv.attr('cbId', cbId);
     return formDiv;
 }
 
@@ -242,7 +255,7 @@ function generateStepJson() {
     let param;
     let selectionType;
     let scriptLanguage;
-    $('#edit-step-parameters .row').each(function() {
+    $('#edit-step-parameters .parameter-form').each(function() {
         selectionType = $(this).find('select').val();
         if (selectionType.indexOf('script') === 0) {
             scriptLanguage = selectionType.split('-')[1];
@@ -253,7 +266,7 @@ function generateStepJson() {
         param = {
             type: selectionType,
             name: $(this).find('input[name="stepParamName"]').val(),
-            required: $(this).find('#' + $(this).attr('cbId')).attr('aria-pressed') === 'true',
+            required: $(this).find('#' + $(this).attr('cbId')).is(':checked'),
             language: scriptLanguage,
             className: setStringValue($(this).find('input[name="stepParamClassName"]').val())
         };
@@ -364,19 +377,11 @@ function stepNeedsSave() {
             let param1;
             _.forEach(step.params, (param) => {
                 param1 = _.find(originalStep.params, p => p.name === param.name);
-                if (!param1) {
-                    diff = true;
-                    return false;
-                } else if (param.type !== param1.type) {
-                    diff = true;
-                    return false;
-                } else if (param.required !== param1.required) {
-                    diff = true;
-                    return false;
-                }  else if (param.language !== param1.language) {
-                    diff = true;
-                    return false;
-                } else if (param.className !== param1.className) {
+                if (!param1 ||
+                    param.type !== param1.type ||
+                    param.required !== param1.required ||
+                    param.language !== param1.language ||
+                    param.className !== param1.className) {
                     diff = true;
                     return false;
                 } else {
